a5cc34658f9917e5fc3fe9e45403ea1e584469a1
Yashwanth Kolli <yashwanthkolli3271gmail.com>
2020-08-28 02:31:20 +0530

    Frontend Variations

--- src/Pages/Round1Score/RoundOneScore.js
+++ src/Pages/Round1Score/RoundOneScore.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux';
 
+import './RoundOneScore.Styles.css'
+
 const RoundOneScore = ({currentUser}) => {
-    console.log(currentUser)
+    const onclick = () => {
+        window.location = 'round2/'+currentUser.currentUser._id
+    }
+
     return (
         <div className='round-one-score-page'>
-            Fuck
+            <div className='round-one-score'>
+                <p className='round-text'>Round-1 Completed!!</p>
+                <p className='score-text'>Your score is</p>
+                <p className='score'>Rs. {currentUser.score}</p>
+                <button onClick={onclick}>Round-2</button>
+            </div>
         </div>
     )
 }
